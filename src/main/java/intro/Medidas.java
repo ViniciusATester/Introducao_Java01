30ac1931c3c53f7fc06d30404b6548ed51568668
Vinicius <viniciusat.13@gmail.com>
2021-11-02 16:19:00 -0300

    Criei e movi os metodos Agora o metodo maind chama os 2 metodos calcularAreaModoCurto e calcularAreaModoExtenso e tambem passaram a ser static
    metodos com swith , if e else, for

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -1,39 +1,106 @@
 package intro;
 
+import java.util.Scanner;
+
 public class Medidas {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-        calcularAreaModoCurto();
-        calcularAreaModoExtenso();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("O P Ç Õ E S");
+        System.out.println("c - calcular area o modo curto");
+        System.out.println("e - Calcular area o modo extenso ");
+        System.out.println("i - If Simples ");
+        System.out.println("d - Contar ate dez ");
+        System.out.println("r - Contagem regrassiva de 10 ate 0 ");
+        System.out.println("Qual opção desejada : ");
+        String opcao = scanner.next();
 
 
+        switch (opcao) {
+            case "i":
+            case "I":
+                System.out.println("Você escolher executar o metodo ifSimpes ");
+                ifSimples();
+                break;
+            case "r":
+            case "R":
+                System.out.println("Você escolher executar o metodo contagemRegressiva ");
+                contagemRegressiva();
+                break;
+            case "d":
+            case "D":
+                System.out.println("Você escolher executar o metodo contarAteDez");
+                contarAteDez();
+                break;
+            case "c":
+            case "C":
+                System.out.println("Você escolher executar o metodo calcularAreaModoCurto ");
+                calcularAreaModoCurto();
+                break;
+            case "e":
+            case "E":
+                System.out.println("Você escolher executar o metodo calcularAreaModoextenso ");
+                calcularAreaModoExtenso();
+                break;
+            default:
+                System.out.println("Você escolher outro valor que não tem uma ação associada ");
+                break;
+        }
+    }
+
+    public static void ifSimples() {
+
+        String modo = "curto";
+
+        if (modo == "curto") {
+            calcularAreaModoCurto();
+        } else {
+            calcularAreaModoExtenso();
+        }
 
     }
-    public static void calcularAreaModoCurto(){
+
+    public static void calcularAreaModoCurto() {
         System.out.println("Calculo de area modo curto");
 
         int largura = 4;
-        int comprimento= 3;
+        int comprimento = 3;
 
-        System.out.println( "A largura de "+largura +"m e o comprimento de "
-                +comprimento+"m a area e de "+largura * comprimento+" m"  );
+        System.out.println("A largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a area e de " + largura * comprimento + " m");
     }
 
-        public static void calcularAreaModoExtenso(){
+    public static void calcularAreaModoExtenso() {
 
-            System.out.println("Calculo de area modo extenso");
-            int largura;
-            int comprimento;
-            int resultado;
+        System.out.println("Calculo de area modo extenso");
+        int largura;
+        int comprimento;
+        int resultado;
 
-            largura = 5;
-            comprimento = 6;
+        largura = 5;
+        comprimento = 6;
 
-            resultado = largura * comprimento;
-            System.out.println( "A largura de "+largura +"m e o comprimento de "
-                    +comprimento+"m a area e de "+resultado+" m"  );
+        resultado = largura * comprimento;
+        System.out.println("A largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a area e de " + resultado + " m");
 
     }
 
+    public static void contarAteDez() {
+        System.out.println("Contagem crescente ");
+        for (int numero = 1; numero <= 10000; numero++) {
+            System.out.println(numero);
+        }
+    }
+
+    public static void contagemRegressiva() {
+        System.out.println("Contagem regressiva ");
+        for (int numero = 10; numero > -1; numero--) {
+            System.out.println(numero);
+        }
+
+    }
 }
+
+
